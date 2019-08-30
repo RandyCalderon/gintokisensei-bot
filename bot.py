0488262365b8f44b649f18468ab85691c90531e1
Randy Calderon <randycweb@gmail.com>
2019-08-29 21:03:53 -0700

    Remove token.txt from git tracking

---
+++ bot.py
@@ -0,0 +1,20 @@
+import discord
+
+
+def read_token():
+    with open("token.txt", "r") as f:
+        lines = f.readlines()
+        print(lines[0].strip())
+        return lines[0].strip()
+
+
+token = read_token()
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    if message.content.find("!hello") != -1:
+        await message.channel.send("Hi")
+
+client.run(token)
