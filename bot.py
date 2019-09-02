157cb805223d7b0fdc4caa8a88ed877958dadfb5
Kunal Mishra <mishrakunal.dev@gmail.com>
2019-09-02 01:45:32 -0400

    Add character of the day functionality

--- bot.py
+++ bot.py
@@ -1,20 +1,24 @@
 import discord
+from charguana import get_charset
+import random
 
-
-def read_token():
-    with open("token.txt", "r") as f:
-        lines = f.readlines()
-        print(lines[0].strip())
-        return lines[0].strip()
-
-
-token = read_token()
+token = "NjE2MDk4ODIyNjQwMjM4NjMw.XWifpA.rhbfgIgZRdBOmJ32ND_U0wCbJ-0"
 
 client = discord.Client()
+
+hiragana = list(get_charset('hiragana'))
 
 @client.event
 async def on_message(message):
     if message.content.find("!hello") != -1:
-        await message.channel.send("Hi")
+        await message.channel.send("ビールとおっぱいがだいすきです！")
+
+    if message.content.find("!WOTD") != -1:
+        rand_num = random.randint(0, len(hiragana) - 1)
+        await message.channel.send(f'Character of the day: {hiragana[rand_num]}')
+
+    if message.content.find("!help") != -1:
+        # TODO: implement
+        pass
 
 client.run(token)
