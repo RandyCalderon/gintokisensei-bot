ce97e5d0f789f68373ddb8292d480cd0b39f28a9
John Smith <sjohn2219@gmail.com>
2019-09-08 00:20:58 -0400

    Change logic for parsing commands
    Add function to read the token from token.txt
    
    Change the description of the !help command

--- bot.py
+++ bot.py
@@ -2,7 +2,12 @@ import discord
 from charguana import get_charset
 import random
 
-token = "NjE2MDk4ODIyNjQwMjM4NjMw.XXG9mw.wpyxaIQJ4LbxQkT0T3cEtm658j8"
+def read_token():
+    with open("token.txt", "r") as token_file:
+        lines = token_file.readlines()
+        return lines[0].strip()
+
+token = read_token()
 
 client = discord.Client()
 
@@ -10,33 +15,32 @@ hiragana = list(get_charset('hiragana'))
 
 @client.event
 async def on_message(message):
-    if message.content.find("!hello") != -1:
+    if message.content.startswith("!hello") == True:
         await message.channel.send("ビールとおっぱいがだいすきです！")
 
-    if message.content.find("!COTD") != -1:
+    if message.content.startswith("!COTD") == True:
         rand_num = random.randint(0, len(hiragana) - 1)
         await message.channel.send(f'Character of the day: {hiragana[rand_num]}')
 
-
-    if message.content.find("!WOTD") != -1:
+    if message.content.startswith("!WOTD") == True:
         pass
 
-    if message.content.find("!POTD") != -1:
+    if message.content.startswith("!POTD") != True:
         pass
 
-    if message.content.find("!help") != -1:
+    if message.content.startswith("!help") == True:
 
-        e = discord.Embed(description='''***Note: Use '!' before any of the commands listed below***
+        e = discord.Embed(description='''
 
         Commands
         -------------
-        ***hello*** - Say hi to Gintoki Sensei
+        ***!hello*** - Say hi to Gintoki Sensei
 
-        ***COTD***  (Character of the day!) - Get the character of the day
+        ***!COTD***   (Character of the day!) - Get the character of the day
 
-        ***WOTD***  (Word of the day!) - Get the word of the day and link to the word pronounciation
+        ***!WOTD***   (Word of the day!) - Get the word of the day and link to the word pronounciation
 
-        ***POTD***  (Phrase of the day!) - Get the phrase of the day''', colour=discord.Colour.teal())
+        ***!POTD***   (Phrase of the day!) - Get the phrase of the day''', colour=discord.Colour.teal())
 
         await message.channel.send(embed=e)
 
